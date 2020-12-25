745c4298d3d8c630e05b4b25aa611dc2dcdbf474
Deepak Nallajerla <deepaknbk@gmail.com>
2020-12-25 08:58:44 -0600

    small fix

--- src/b5_names.py
+++ src/b5_names.py
@@ -1,17 +1,19 @@
-import collections
+from collections import defaultdict
 NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
          'julian sequeira', 'sandra bullock', 'keanu reeves',
          'julbob pybites', 'bob belderbos', 'julian sequeira',
          'al pacino', 'brad pitt', 'matt damon', 'brad pitt']
 
 
-def dedup_and_title_case_names():
+def dedup_and_title_case_names(names):
     """Should return a list of title cased names,
        each name appears only once"""
-    dedup_names=defaultdict(list)
-    dedup_title_names=[name.title() for name in NAMES if name not in dedup_names.append(name)]
-    print(dedup_title_names)
-    pass
+    dedup_names=[]
+    title_names=[name.title() for name in names]
+    for name in title_names:
+        if name not in dedup_names:
+            dedup_names.append(name)
+    return dedup_names
 
 
 def sort_by_surname_desc(names):
@@ -27,4 +29,5 @@ def shortest_first_name(names):
     names = dedup_and_title_case_names(names)
     # ...
 
-dedup_and_title_case_names()
+x=dedup_and_title_case_names(NAMES)
+print(x)
