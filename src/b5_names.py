e39e57f014a8e6556d5f98691290dabbfa9448cf
Deepak Nallajerla <deepaknbk@gmail.com>
2020-12-25 16:20:05 -0600

    pybite 5 completed

--- src/b5_names.py
+++ src/b5_names.py
@@ -16,18 +16,25 @@ def dedup_and_title_case_names(names):
     return dedup_names
 
 
+def last_nm(name):
+    return name.split()[1]
+
+def first_nm_len(name):
+    return len(name.split()[0])
+
 def sort_by_surname_desc(names):
     """Returns names list sorted desc by surname"""
     names = dedup_and_title_case_names(names)
-    # ...
-
+    return sorted(names,key=last_nm,reverse=True)
+    #pass
 
 def shortest_first_name(names):
     """Returns the shortest first name (str).
        You can assume there is only one shortest name.
     """
     names = dedup_and_title_case_names(names)
+    return sorted(names, key=first_nm_len)[0].split()[0]
     # ...
 
-x=dedup_and_title_case_names(NAMES)
-print(x)
+print("Sorted based on the Sur Name:",sort_by_surname_desc(NAMES))
+print("Shortest First Name:",shortest_first_name(NAMES))
