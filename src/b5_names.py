80b5754a3025e859ed71268f6af43b162d355be0
Deepak Nallajerla <deepaknbk@gmail.com>
2020-12-25 08:44:01 -0600

    bite 5 challange

---
+++ src/b5_names.py
@@ -0,0 +1,30 @@
+import collections
+NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
+         'julian sequeira', 'sandra bullock', 'keanu reeves',
+         'julbob pybites', 'bob belderbos', 'julian sequeira',
+         'al pacino', 'brad pitt', 'matt damon', 'brad pitt']
+
+
+def dedup_and_title_case_names():
+    """Should return a list of title cased names,
+       each name appears only once"""
+    dedup_names=defaultdict(list)
+    dedup_title_names=[name.title() for name in NAMES if name not in dedup_names.append(name)]
+    print(dedup_title_names)
+    pass
+
+
+def sort_by_surname_desc(names):
+    """Returns names list sorted desc by surname"""
+    names = dedup_and_title_case_names(names)
+    # ...
+
+
+def shortest_first_name(names):
+    """Returns the shortest first name (str).
+       You can assume there is only one shortest name.
+    """
+    names = dedup_and_title_case_names(names)
+    # ...
+
+dedup_and_title_case_names()
